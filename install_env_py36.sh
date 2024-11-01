7026b73ff6399140e5bfd8b49914ec046539e4ae
qin-yuhao <2365517364@qq.com>
2024-11-01 14:22:45 +0800

    secd

--- install_env_py36.sh
+++ install_env_py36.sh
@@ -1,5 +1,6 @@
 #打印
 echo "start install env"
+pip uninstall tensorflow-gpu
 pip install pyglet==1.5.1 -i https://pypi.tuna.tsinghua.edu.cn/simple
 pip install opencv-python==4.5.4.58 -i https://pypi.tuna.tsinghua.edu.cn/simple
 pip install torch -i https://pypi.tuna.tsinghua.edu.cn/simple
@@ -7,6 +8,5 @@ pip install tensorflow -i https://pypi.tuna.tsinghua.edu.cn/simple
 pip install matplotlib -i https://pypi.tuna.tsinghua.edu.cn/simple
 pip install pandas -i https://pypi.tuna.tsinghua.edu.cn/simple
 pip install gym==0.10.4 -i https://pypi.tuna.tsinghua.edu.cn/simple  
-
 
 python3 l_pole_36.py 
